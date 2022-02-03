57c448d9e8355098fa8d00a13a60337c6c41a1dc
rivques <38469076+rivques@users.noreply.github.com>
2022-02-03 17:36:23 -0500

    implement more stuff

--- main.py
+++ main.py
@@ -1,44 +1,75 @@
+from gcodeinstruction import GcodeInstruction
 from util import routines, tools, utils
-from util.agent import Vector, VirxERLU, run_bot
+from util.agent import Vector, VirxERLU, game_object, run_bot
+from rlbot.utils.game_state_util import GameState, BallState, CarState, Physics, Vector3, Rotator
 import logging
 import re
-
-
 class Bot(VirxERLU):
-    # If the bot encounters an error, VirxERLU will do it's best to keep the bot from crashing.
-    # VirxERLU uses a stack system for it's routines. A stack is a first-in, last-out system of routines.
-    # VirxERLU on VirxEC Showcase -> https://virxerlu.virxcase.dev/
-    # Questions? Want to be notified about VirxERLU updates? Join my Discord -> https://discord.gg/5ARzYRD2Na
-    # Wiki -> https://github.com/VirxEC/VirxERLU/wiki
     def init(self):
         # NOTE This method is ran only once, and it's when the bot starts up
         self.logger = logging.getLogger("SNOWBOT")
         self.logger.setLevel(logging.DEBUG)
+        # create console handler and set level to debug
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
 
-        self.layer_end_re = re.compile(r"^G(1|0)(?=.*Z(\d+(\.\d*)))", flags=[re.IGNORECASE])
-        self.line_re = re.compile(r"^G(1|0)(?=.*x(\d+(\.\d*)))(?=.*y(\d+(\.\d*))).*", flags=[re.IGNORECASE])
-        self.type_re = re.compile(r";TYPE:(.*)", flags=[re.IGNORECASE])
-    
-    def load_gcode(filepath):
+        # create formatter
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        # add formatter to ch
+        ch.setFormatter(formatter)
+
+        # add ch to logger
+        self.logger.addHandler(ch)
+        self.logger.info("Snowbot init!")
+        self.layer_end_re = re.compile(r"^G(1|0)(?=.*Z(\d+(\.\d*)))", flags=re.IGNORECASE)
+        
+    def load_gcode(self, filepath):
         layer_started = False
         self.logger.info(f"Loading gcode file {filepath}...")
+        self.instructions = []
         with open(filepath) as f:
-            for line in f.readlines():
+            for i, line in enumerate(f.readlines()):
+                line = line.strip()
                 layer_mo = self.layer_end_re.match(line)
-                if not layer_started and layer_mo and int(layer_mo.group(2)) == 0.2:
+                if not layer_started and layer_mo and layer_mo.group(2) == "0.2":
                     layer_started = True
-                    self.logger.info(f"First line of layer found: {line}")
+                    self.logger.info(f"First line of layer found (#{i+1}): {line}")
                     continue
-                if layer_mo and int(layer_mo.group(2)) == 0.4:
-                    self.logger.info(f"Last line of layer found: {line}")
+                if not layer_started:
+                    continue
+                if layer_mo and layer_mo.group(2) == "0.4":
+                    self.logger.info(f"Last line of layer found (#{i+1}): {line}")
                     break
-                    
+                self.instructions.append(GcodeInstruction(line, i))
+        
+        self.current_instruction = None
+        self.current_instruction_num = -1
+        self.current_gcode_type = None
 
     def run(self):
         # NOTE This method is ran every tick
         if len(self.friends) > 0 or len(self.enemies) > 0:
             self.logger.warn("There are other cars on the field!")
-        
+        if self.is_clear():
+            # time for the next instruction
+            self.current_instruction_num += 1
+            self.current_instruction = self.instructions[self.current_instruction_num]
+            while not self.current_instruction.valid:
+                self.current_instruction_num += 1
+                self.current_instruction = self.instructions[self.current_instruction_num]
+            if self.current_instruction.type is not None:
+                self.current_gcode_type = self.current_instruction.type
+            elif self.current_instruction.is_travel:
+                car_state = CarState(physics=Physics(location=Vector3(self.current_instruction.y,self.current_instruction.y, None)))
+                game_state = GameState(cars={self.index: car_state})
+                self.set_game_state(game_state)
+            else:
+                # get a rotator pointing toward our destination
+                # point us that way
+                # go there
+                self.push(routines.goto())
+
 
     def demolished(self):
         # NOTE This method is ran every tick that your bot it demolished
